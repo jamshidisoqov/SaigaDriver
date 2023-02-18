2d0d8ef8424d74b6e826b4b2c8ff57a31f552311
jamshidisoqov <isoqovjamshid01@gmail.com>
2023-02-18 13:50:35 +0500

    alerter added

--- app/src/main/java/uz/gita/saiga_driver/MainActivity.kt
+++ app/src/main/java/uz/gita/saiga_driver/MainActivity.kt
@@ -2,20 +2,31 @@ package uz.gita.saiga_driver
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.maps.model.LatLng
 import com.google.gson.Gson
+import com.tapadoo.alerter.Alerter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import uz.gita.saiga_driver.app.App
+import kotlinx.coroutines.launch
 import uz.gita.saiga_driver.data.remote.response.order.OrderResponse
+import uz.gita.saiga_driver.domain.repository.MapRepository
+import uz.gita.saiga_driver.domain.repository.OrderRepository
 import uz.gita.saiga_driver.navigation.NavigationHandler
 import uz.gita.saiga_driver.presentation.dialogs.ProgressDialog
 import uz.gita.saiga_driver.services.notification.NotificationService
+import uz.gita.saiga_driver.utils.NUKUS
+import uz.gita.saiga_driver.utils.currentLocation
+import uz.gita.saiga_driver.utils.extensions.calculationByDistance
+import uz.gita.saiga_driver.utils.extensions.combine
 import uz.gita.saiga_driver.utils.extensions.toJsonData
 import javax.inject.Inject
 
@@ -26,6 +37,14 @@ class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var navigationHandler: NavigationHandler
+
+    @Inject
+    lateinit var repository: MapRepository
+
+    @Inject
+    lateinit var orderRepository: OrderRepository
+
+    private lateinit var controller: NavController
 
     @Inject
     lateinit var gson: Gson
@@ -43,6 +62,11 @@ class MainActivity : AppCompatActivity() {
             .onEach { it.invoke(fragment.findNavController()) }
             .launchIn(lifecycleScope)
         dialog = ProgressDialog(this)
+
+
+
+        controller = fragment.navController
+
     }
 
     fun showProgress() {
@@ -57,9 +81,56 @@ class MainActivity : AppCompatActivity() {
         lateinit var activity: MainActivity
     }
 
-    fun createNotification(orderResponse: OrderResponse){
+    fun createNotification(orderResponse: OrderResponse) {
+        showAlerter(orderResponse)
         val intent = Intent(this, NotificationService::class.java)
-        intent.putExtra("order",gson.toJsonData(orderResponse))
+        intent.putExtra("order", gson.toJsonData(orderResponse))
         startService(intent)
+    }
+
+    private fun showAlerter(orderResponse: OrderResponse) {
+
+        lifecycleScope.launch {
+
+
+            val fromAddress = with(orderResponse.direction.addressFrom) {
+                LatLng(lat!!, lon!!)
+            }
+
+            repository.getAddressByLocation(fromAddress).collectLatest { result ->
+                result.onSuccess {
+                    Alerter.create(this@MainActivity)
+                        .setTitle(it)
+                        .setText(
+                            "Distance".combine(
+                                calculationByDistance(
+                                    fromAddress,
+                                    currentLocation.value ?: NUKUS
+                                ).toString()
+                            )
+                        )
+                        .setDuration(5000)
+                        .setIcon(R.drawable.saiga_yellow_car)
+                        .setIconSize(R.dimen.icon_size_alerter)
+                        .setIconColorFilter(Color.TRANSPARENT)
+                        .addButton("Accept") {
+                            lifecycleScope.launch {
+                                orderRepository.receiveOrder(orderResponse.id)
+                                    .collectLatest { result ->
+                                        result.onSuccess {
+                                            controller.navigate(
+                                                NavGraphDirections.actionGlobalTripScreen(
+                                                    orderResponse
+                                                )
+                                            )
+                                        }
+                                    }
+                            }
+                        }
+                        .setBackgroundColorInt(Color.parseColor("#975C28"))
+                        .show()
+                }
+            }
+        }
     }
 }
